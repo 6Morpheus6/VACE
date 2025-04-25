99577458d5cfff1d3588424701d31aff69309cf2
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-04-25 18:31:22 +0200

    cross-platform compliant save name

--- vace/gradios/vace_wan_demo.py
+++ vace/gradios/vace_wan_demo.py
@@ -214,7 +214,7 @@ class VACEInference:
             seed=infer_seed,
             offload_model=True)
 
-        name = '{0:%Y%m%d%-H%M%S}'.format(datetime.datetime.now())
+        name = '{0:%Y%m%d%H%M%S}'.format(datetime.datetime.now())
         video_path = os.path.join(self.save_dir, f'cur_gallery_{name}.mp4')
         video_frames = (torch.clamp(video / 2 + 0.5, min=0.0, max=1.0).permute(1, 2, 3, 0) * 255).cpu().numpy().astype(np.uint8)
 
